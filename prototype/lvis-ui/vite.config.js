76bfede6c806eb9e00d86905b683ea27b2e04cc6
Hoang Do Minh <thaihv@gmail.com>
2024-11-19 15:33:06 +0700

    update nginx config

--- prototype/lvis-ui/vite.config.js
+++ prototype/lvis-ui/vite.config.js
@@ -4,6 +4,10 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  root: './',  
+  build: {
+    outDir: 'dist',
+  },  
   css: {
     preprocessorOptions: {
       scss: {
